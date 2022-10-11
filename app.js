0c1e7d6d73b995843f56db1c594c04217953c521
thompsongiovanni <thompson_giovanni@columbusstate.edu>
2022-10-11 20:43:10 +0000

    Added model.js and mongodb connection

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 var express = require('express');
+var mongoose = require('mongoose')
 var app = express();
 
-app.use('/static', express.static("public"))
-app.set("view engine", "ejs")
+app.use('/static', express.static("public"));
+app.use(express.urlencoded({ extended: true}))
+app.set("view engine", "ejs");
 
-app.get('/hello', function(req, res){
-    res.send('Hello World!');
-})
+const mongoDB = 'mongodb+srv://thompson_giovanni:Trillchains12345!!!@cluster0.ktpcdic.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, "MongoDB Connection Error:"))
+
 
 app.get('/', function(req, res){
     res.render('todo.ejs');
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body.content)
 })
 
 app.listen(3000, function(){
