1c3556286b2fac94004930787d7b5506393f10ab
thompsongiovanni <thompson_giovanni@columbusstate.edu>
2022-10-11 20:06:50 +0000

    added more css and folders

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
+app.use('/static', express.static("public"))
+app.set("view engine", "ejs")
+
+app.get('/hello', function(req, res){
     res.send('Hello World!');
+})
+
+app.get('/', function(req, res){
+    res.render('todo.ejs');
 })
 
 app.listen(3000, function(){
