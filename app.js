e9a0d18834ad65a3ce093e79b1157552b6320283
thompsongiovanni <thompson_giovanni@columbusstate.edu>
2022-10-14 20:39:59 +0000

    ....

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 var express = require('express');
 var mongoose = require('mongoose')
+var axios = require('axios').default;
 var app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/static', express.static("public"));
-app.use(express.urlencoded({ extended: true}))
 app.set("view engine", "ejs");
 const Todo = require('./models/todo.model');
-const mongoDB = 'mongodb+srv://thompson_giovanni:Trillchains12345!!!@cluster0.ktpcdic.mongodb.net/?retryWrites=true&w=majority'
+const mongoDB = 'mongodb+srv://canedo_jose:Un6x6HqC2a1MHQCD@cluster0.pkudmic.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
-db.on('error', console.error.bind(console, "MongoDB Connection Error:"))
+db.on('error', console.error.bind(console, "MongoDb connection error: "))
 
 
 app.get('/', function(req, res){
-    res.render('todo.ejs');
+    axios.get('https://xkcd.com/info.0.json').then(function(response){
+        Todo.find(function(err, todo){
+            if(err){
+                res.json({"Error: ": err})
+            } else {
+                res.render('todo.ejs', {todoList: todo, comicData: response.data});
+            }
+        })
+    }).catch(function(error){
+        res.json({"Error: ": error})
+    })
+    // api call by comic number https://xkcd.com/614/info.0.json number (614) randomly generated
+    // min 1, max 2682 
+    
 })
-//Creates item in DB
-app.post('/', (req, res) => {
+// Creates item in DB
+app.post('/create', (req, res) => {
     let newTodo = new Todo({
         todo: req.body.content,
         done: false
@@ -27,24 +43,26 @@ app.post('/', (req, res) => {
         if(err){
             res.json({"Error: ": err})
         } else {
-            res.json({"Status: ": "Successful", "ObjectId": todo.id})
+            res.redirect('/');
         }
     })
 })
-//Modifies item in DB
-app.put('/', (req, res) => {
-    let id = req.body.check;
-    let err = {}
+// Modifies item in DB
+app.put('/done', (req, res) => {
+    let id = req.body.id;
+    let err = null
     if(typeof id === "string"){
         Todo.updateOne({_id: id}, {done: true}, function(error){
             if(error){
+                console.log(error)
                 err = error
             }
         })
     } else if (typeof id === "object") {
         id.forEach( ID => {
-            Todo.updateOne({_id: ID}, {done: true}, function(error){
+            Todo.updateOne({_id: id}, {done: true}, function(error){
                 if(error){
+                    console.log(error)
                     err = error
                 }
             })
@@ -53,13 +71,13 @@ app.put('/', (req, res) => {
     if(err){
         res.json({"Error: ": err})
     } else {
-        res.json({"Status: ": "Successful"})
+        res.redirect('/');
     }
 })
 
-app.delete('/', (req, res) => {
-    let id = req.body.check;
-    let err = {}
+app.delete('/delete/:id', (req, res) => {
+    let id = req.params.id;
+    let err;
     if(typeof id === "string"){
         Todo.deleteOne({_id: id}, function(error){
             if(error){
@@ -78,7 +96,7 @@ app.delete('/', (req, res) => {
     if(err){
         res.json({"Error: ": err})
     } else {
-        res.json({"Status: ": "Successful"})
+        res.redirect('/');
     }
 })
 
