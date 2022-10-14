89d6e32a08cb0b6bfa53ea32e7a7264a48b8f4df
thompsongiovanni <thompson_giovanni@columbusstate.edu>
2022-10-14 19:48:12 +0000

    added code to modify and create mongodb

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ db.on('error', console.error.bind(console, "MongoDB Connection Error:"))
 app.get('/', function(req, res){
     res.render('todo.ejs');
 })
-
+//Creates item in DB
 app.post('/', (req, res) => {
     let newTodo = new Todo({
         todo: req.body.content,
@@ -30,6 +30,56 @@ app.post('/', (req, res) => {
             res.json({"Status: ": "Successful", "ObjectId": todo.id})
         }
     })
+})
+//Modifies item in DB
+app.put('/', (req, res) => {
+    let id = req.body.check;
+    let err = {}
+    if(typeof id === "string"){
+        Todo.updateOne({_id: id}, {done: true}, function(error){
+            if(error){
+                err = error
+            }
+        })
+    } else if (typeof id === "object") {
+        id.forEach( ID => {
+            Todo.updateOne({_id: ID}, {done: true}, function(error){
+                if(error){
+                    err = error
+                }
+            })
+        })
+    }
+    if(err){
+        res.json({"Error: ": err})
+    } else {
+        res.json({"Status: ": "Successful"})
+    }
+})
+
+app.delete('/', (req, res) => {
+    let id = req.body.check;
+    let err = {}
+    if(typeof id === "string"){
+        Todo.deleteOne({_id: id}, function(error){
+            if(error){
+                err = error
+            }
+        })
+    } else if (typeof id === "object") {
+        id.forEach( ID => {
+            Todo.deleteOne({_id: ID}, function(error){
+                if(error){
+                    err = error
+                }
+            })
+        })
+    }
+    if(err){
+        res.json({"Error: ": err})
+    } else {
+        res.json({"Status: ": "Successful"})
+    }
 })
 
 app.listen(3000, function(){
