40dc1e6a42e4b5869f80a8eb1a0bfa613cc33dd5
thompsongiovanni <thompson_giovanni@columbusstate.edu>
2022-11-21 23:15:25 +0000

    done

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var mongoose = require('mongoose')
 var axios = require('axios');
 var app = express();
 const bodyParser = require('body-parser');
@@ -9,97 +8,16 @@ app.use(bodyParser.json());
 
 app.use('/static', express.static("public"));
 app.set("view engine", "ejs");
-const Todo = require('./models/todo.model');
-const mongoDB = 'mongodb+srv://thompson_giovanni:Trillchains12345!!!@cluster0.ktpcdic.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, "MongoDB connection error: "))
 
 
 app.get('/', function(req, res){
-    axios.get('https://xkcd.com/info.0.json').then(function(response){
-        Todo.find(function(err, todo){
-            if(err){
-                res.json({"Error: ": err})
-            } else {
-                res.render('todo.ejs', {todoList: todo, comicData: response.data});
-            }
-        })
-    }).catch(function(error){
-        res.json({"Error: ": error})
-    })
-    // api call by comic number https://xkcd.com/614/info.0.json number (614) randomly generated
-    // min 1, max 2682 
-    
-})
-// Creates item in DB
-app.post('/create', (req, res) => {
-    let newTodo = new Todo({
-        todo: req.body.content,
-        done: false
-    })
-    newTodo.save(function(err, todo){
-        if(err){
-            res.json({"Error: ": err})
-        } else {
-            res.redirect('/');
-        }
-    })
-})
-// Modifies item in DB
-app.put('/done', (req, res) => {
-    let id = req.body.id;
-    let err = null
-    console.log(req.body)
-    if(typeof id === "string"){
-        Todo.updateOne({_id: id}, {done: true}, function(error){
-            if(error){
-                console.log(error)
-                err = error
-             }
-        })
-    } else if (typeof id === "object") {
-        id.forEach( ID => {
-            Todo.updateOne({_id: id}, {done: true}, function(error){
-                if(error){
-                    console.log(error)
-                    err = error
-                }
-            })
-        })
-    }
-    if(err){
-        res.json({"Error: ": err})
-    } else {
-        res.redirect('/');
-    }
-})
+    random = Math.floor(Math.random() * 2500) +1;
 
-app.delete('/delete/:id', (req, res) => {
-    let id = req.params.id;
-    let err;
-    if(typeof id === "string"){
-        Todo.deleteOne({_id: id}, function(error){
-            if(error){
-                err = error
-            }
-        })
-    } else if (typeof id === "object") {
-        id.forEach( ID => {
-            Todo.deleteOne({_id: ID}, function(error){
-                if(error){
-                    err = error
-                }
-            })
-        })
-    }
-    if(err){
-        res.json({"Error: ": err})
-    } else {
-        res.redirect('/');
-    }
-})
+let imgURL = `https://xkcd.com/${random}/info.0.json`;
+    axios.get(imgURL).then(function (response) {
+        res.render('todo.ejs', { comicData: response.data });
+    });
+});   
 
 app.listen(3000, function(){
     console.log('App listening on port 3000')
